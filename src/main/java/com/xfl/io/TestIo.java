8bc4960ed954bff2faaf61078830abbf60f69414
XFL <18720909751@163.com>
2016-11-01 22:49:44 +0800

    字符串String转Byte

--- src/main/java/com/xfl/io/TestIo.java
+++ src/main/java/com/xfl/io/TestIo.java
@@ -1,5 +1,7 @@
 package com.xfl.io;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by XFL
  * time on 2016/11/1 20:54
@@ -10,5 +12,19 @@ public class TestIo {
     }
     public static void  main(String []args){
         System.out.println("Test Io");
+        testBytes("XFL宁都");
+    }
+    private static void testBytes(String str){
+        if (str != null){
+            try {
+                byte[] bytes = str.getBytes("GBK");
+                for (byte aByte : bytes) {
+                    System.out.print(aByte + ":");
+                }
+            }  catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
