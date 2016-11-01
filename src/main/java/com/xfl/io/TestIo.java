55f5cd22b40389ff3474939a0f2202cf64ec087f
XFL <18720909751@163.com>
2016-11-01 20:59:44 +0800

    测试

---
+++ src/main/java/com/xfl/io/TestIo.java
@@ -0,0 +1,14 @@
+package com.xfl.io;
+
+/**
+ * Created by XFL
+ * time on 2016/11/1 20:54
+ * description:
+ */
+public class TestIo {
+    public TestIo() {
+    }
+    public static void  main(String []args){
+        System.out.println("Test Io");
+    }
+}
