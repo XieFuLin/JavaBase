bad8dfd67827a408bef607f527c3ebe2f075529a
XFL <18720909751@163.com>
2016-12-03 21:36:20 +0800

    增加注释

--- src/main/java/com/xfl/exception/TestException.java
+++ src/main/java/com/xfl/exception/TestException.java
@@ -4,6 +4,8 @@ package com.xfl.exception;
  * Created by XFL.
  * time on 2016/12/3 15:14
  * description:异常捕获测试
+ * 在执行finally代码时，如果finally代码本身没有退出的语句（return或抛出异常），
+ * finally执行完毕后还会返回try或catch，由try或catch执行退出指令
  */
 public class TestException {
     public static void main(String[] args) {
@@ -27,10 +29,21 @@ public class TestException {
             int c = a / b;
         } catch (Exception e) {
             temp += 1;
-            //执行return之前会先执行finally
             System.out.println(temp);
+            /**
+             * Return并不是让函数马上返回，而是return语句执行后，将把返回结果放置进函数栈中，
+             * 此时函数并不是马上返回，它要执行finally语句后才真正开始返回。
+             * finally语句是在try的return语句执行之后，return返回之前执行
+             */
             return temp;
         } finally {
+            /**
+             * finally不是一定会执行的.
+             * (1）try语句没有被执行到，如在try语句之前就返回了，这样finally语句就不会执行，
+             * 这也说明了finally语句被执行的必要而非充分条件是：相应的try语句一定被执行到。
+             *（2）在try块中有System.exit(0);这样的语句，System.exit(0);是终止Java虚拟机JVM的，
+             * 连JVM都停止了，所有都结束了，当然finally语句也不会被执行到。
+             */
             temp += 1;
             System.out.println(temp);
         }
