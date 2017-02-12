e4548535419d899c728696f03c6085156ee6e2be
XFL <18720909751@163.com>
2017-02-12 23:59:54 +0800

    小数计算问题

--- src/main/java/com/xfl/base/TestBigDecimal.java
+++ src/main/java/com/xfl/base/TestBigDecimal.java
@@ -1,6 +1,7 @@
 package com.xfl.base;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * Created by XFL
@@ -9,10 +10,27 @@ import java.math.BigDecimal;
  */
 public class TestBigDecimal {
     public static void main(String[] args) {
-        System.out.println(0.06 + 0.01);
-        System.out.println(1.0 - 0.42);
-        System.out.println(4.015 * 100);
-        System.out.println(303.1 / 1000);
+//        System.out.println(0.06 + 0.01);
+//        System.out.println(1.0 - 0.42);
+//        System.out.println(4.015 * 100);
+//        System.out.println(303.1 / 1000);
         System.out.println(new BigDecimal("0.06"));
+        BigDecimal a1 = new BigDecimal("123.254");
+        BigDecimal a2 = new BigDecimal("0.75");
+        System.out.println(a1.scale());
+        //除
+        System.out.println(a1.divide(a2, 2));
+        //加
+        System.out.println(a1.add(a2));
+        //成
+        System.out.println(a1.multiply(a2));
+        System.out.println(a1.subtract(a2));
+        System.out.println(new BigDecimal("2.3").setScale(2));
+        //只入不舍
+        System.out.println(a1.setScale(2, RoundingMode.UP));
+        //只舍不入
+        System.out.println(a1.setScale(2, RoundingMode.DOWN));
+        //四舍五入
+        System.out.println(a1.setScale(2, RoundingMode.HALF_UP));
     }
 }
