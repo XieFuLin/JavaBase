799971cc71d8bb592b5132b3fa4639d086f80b85
XFL <18720909751@163.com>
2017-02-12 23:33:56 +0800

    Map测试

---
+++ src/main/java/com/xfl/collection/TestMap.java
@@ -0,0 +1,20 @@
+package com.xfl.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by XFL.
+ * time on 2016/12/10 18:56
+ * description:Map相关原理剖析.
+ */
+public class TestMap {
+    public static void main(String[] args) {
+
+    }
+
+    private static void test1() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("key", "value");
+    }
+}
