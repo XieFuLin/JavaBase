0ba0db58bf09603cd638dd31735ce8541956b265
XFL <18720909751@163.com>
2017-02-25 17:05:22 +0800

    double相减问题

--- src/main/java/com/xfl/base/TestBigDecimal.java
+++ src/main/java/com/xfl/base/TestBigDecimal.java
@@ -2,6 +2,7 @@ package com.xfl.base;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 /**
  * Created by XFL
@@ -15,7 +16,7 @@ public class TestBigDecimal {
 //        System.out.println(4.015 * 100);
 //        System.out.println(303.1 / 1000);
         System.out.println(new BigDecimal("0.06"));
-        BigDecimal a1 = new BigDecimal("123.254");
+        BigDecimal a1 = new BigDecimal("123.2562");
         BigDecimal a2 = new BigDecimal("0.75");
         System.out.println(a1.scale());
         //除
@@ -27,10 +28,24 @@ public class TestBigDecimal {
         System.out.println(a1.subtract(a2));
         System.out.println(new BigDecimal("2.3").setScale(2));
         //只入不舍
-        System.out.println(a1.setScale(2, RoundingMode.UP));
+        System.out.println("只入不舍" + a1.setScale(2, RoundingMode.UP));
         //只舍不入
-        System.out.println(a1.setScale(2, RoundingMode.DOWN));
+        System.out.println("只舍不入" + a1.setScale(2, RoundingMode.DOWN));
         //四舍五入
-        System.out.println(a1.setScale(2, RoundingMode.HALF_UP));
+        System.out.println("四舍五入" + a1.setScale(2, RoundingMode.HALF_UP));
+        String number = "89" + "";
+        DecimalFormat df2 = (DecimalFormat) DecimalFormat.getInstance();
+        df2.applyPattern("0.00");
+        System.out.println(df2.format(Double.parseDouble(number)));
+        double d1 = 30.29;
+        double d2 = 1.01;
+        BigDecimal a3 = new BigDecimal(d1 + "");
+        BigDecimal a4 = new BigDecimal(d2 + "");
+        System.out.println(a3.subtract(a4));
+        System.out.println(30.29 - 1.01);
+        System.out.println(new BigDecimal(d1));
+        System.out.println(new BigDecimal(d2));
+        System.out.println(d1 - d2);
+        System.out.println(new BigDecimal(d1).subtract(new BigDecimal(d2)));
     }
 }
