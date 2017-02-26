b7d7341958dbba06007b5d11a4dd26de07ddaa25
XFL <18720909751@163.com>
2017-02-26 23:38:47 +0800

    继承与类的加载顺序

---
+++ src/main/java/com/xfl/base/Base.java
@@ -0,0 +1,30 @@
+package com.xfl.base;
+
+/**
+ * Created by XFL
+ * time on 2017/2/26 23:34
+ * description:
+ */
+public class Base {
+    private String baseName = "base";
+
+    public Base() {
+        callName();
+    }
+
+    public void callName() {
+        System.out.println(baseName);
+    }
+
+    static class Sub extends Base {
+        private String baseName = "sub";
+
+        public void callName() {
+            System.out.println(baseName);
+        }
+    }
+
+    public static void main(String[] args) {
+        Base b = new Sub();
+    }
+}
