56ae77a865f9c6a20b4dd27e5a40ae031d6c0c03
XFL <18720909751@163.com>
2017-05-14 22:28:20 +0800

    消费者实现类

---
+++ src/main/java/com/xfl/rpc/Consumer.java
@@ -0,0 +1,66 @@
+package com.xfl.rpc;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.net.Socket;
+
+/**
+ * Created by XFL
+ * time on 2017/5/14 22:12
+ * description:消费者，调用服务
+ */
+public class Consumer {
+    public static <T> T refer(final Class<T> interfaceClass,
+                              final String host, final int port) throws Exception {
+        if (interfaceClass == null)
+            throw new IllegalArgumentException("Interface class == null");
+        if (!interfaceClass.isInterface())
+            throw new IllegalArgumentException("The " + interfaceClass.getName() + " must be interface class!");
+        if (host == null || host.length() == 0)
+            throw new IllegalArgumentException("Host == null!");
+        if (port <= 0 || port > 65535)
+            throw new IllegalArgumentException("Invalid port " + port);
+        System.out.println("Get remote service " + interfaceClass.getName() + " from server " + host + ":" + port);
+        //动态代理(消费者端关键代码)
+        return (T) Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class<?>[]{interfaceClass}, new InvocationHandler() {
+            public Object invoke(Object proxy, Method method, Object[] arguments) throws Throwable {
+                Socket socket = new Socket(host, port);
+                try {
+                    ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+                    try {
+                        output.writeUTF(method.getName());
+                        output.writeObject(method.getParameterTypes());
+                        output.writeObject(arguments);
+                        ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+                        try {
+                            Object result = input.readObject();
+                            if (result instanceof Throwable) {
+                                throw (Throwable) result;
+                            }
+                            return result;
+                        } finally {
+                            input.close();
+                        }
+                    } finally {
+                        output.close();
+                    }
+                } finally {
+                    socket.close();
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) throws Exception {
+        //获取代理方法
+        HelloService service = refer(HelloService.class, "127.0.0.1", 1234);
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            String hello = service.hello("World" + i);
+            System.out.println(hello);
+            Thread.sleep(1000);
+        }
+    }
+}
