c1f26a2bcefb8ad81e84e91b943a6ddb90f4ae55
XFL <18720909751@163.com>
2017-05-14 22:26:41 +0800

    RPC服务提供者

---
+++ src/main/java/com/xfl/rpc/Provider.java
@@ -0,0 +1,35 @@
+package com.xfl.rpc;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by XFL
+ * time on 2017/5/14 21:22
+ * description:生产者暴露服务
+ */
+public class Provider {
+    public static void export(final Object service, int port) throws Exception {
+        //服务校验
+        if (service == null) {
+            throw new IllegalArgumentException("service must not be null");
+        }
+        //端口校验
+        if (port <= 0 || port > 65535) {
+            throw new IllegalArgumentException("Invalid port:" + port + "a valid port must between 0 and 65535");
+        }
+        //向操作系统注册服务
+        ServerSocket serverSocket = new ServerSocket(port);
+        //循环启动监听
+        while (true) {
+            Socket socket = serverSocket.accept();
+            //开启独立的线程处理服务调用
+            new ServerThread(socket, service).start();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        HelloService service = new HelloServiceImpl();
+        export(service, 1234);
+    }
+}
