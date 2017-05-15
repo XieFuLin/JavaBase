b194aa51bbdf505b9b5bad85677dbbd7a05c4363
XFL <18720909751@163.com>
2017-05-15 23:17:02 +0800

    修改代码注释

--- src/main/java/com/xfl/proxy/ProxyHandler.java
+++ src/main/java/com/xfl/proxy/ProxyHandler.java
@@ -15,6 +15,13 @@ public class ProxyHandler implements InvocationHandler {
         this.proxied = proxied;
     }
 
+    /**
+     * @param proxy  指代我们所代理的那个真实对象.
+     * @param method 指代的是我们所要调用真实对象的某个方法的Method对象
+     * @param args   指代的是调用真实对象某个方法时接受的参数
+     * @return
+     * @throws Throwable
+     */
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         //在转调具体目标对象之前，可以执行一些功能处理
 
