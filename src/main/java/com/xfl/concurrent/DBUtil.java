aa0bfc3e4df014ec0014fe4f6e883126bad68f07
XFL <18720909751@163.com>
2017-05-17 00:32:48 +0800

    使用ThreadLocal为每个线程保留一份Connection变量副本

--- src/main/java/com/xfl/concurrent/DBUtil.java
+++ src/main/java/com/xfl/concurrent/DBUtil.java
@@ -17,26 +17,37 @@ public class DBUtil {
     private static final String password = "618033988";
     // 定义一个数据库连接
     private static Connection conn = null;
-
+    // 定义一个用于放置数据库连接的局部线程变量（使每个线程都拥有自己的连接）
+    private static ThreadLocal<Connection> connContainer = new ThreadLocal<Connection>();
     // 获取连接
     public static Connection getConnection() {
+        //从ThreadLocal中获取Connection
+        Connection conn = connContainer.get();
         try {
             Class.forName(driver);
             conn = DriverManager.getConnection(url, username, password);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            //将 connContainer放入到ThreadLocal中
+            connContainer.set(conn);
         }
         return conn;
     }
 
     // 关闭连接
     public static void closeConnection() {
+        //获取Connection
+        Connection conn = connContainer.get();
         try {
             if (conn != null) {
                 conn.close();
             }
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            //移除Connection
+            connContainer.remove();
         }
     }
 }
